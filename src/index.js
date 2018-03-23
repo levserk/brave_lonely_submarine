731ec1bd649aac809fc2f5989f34ad6fe9b444e8
levserk <levserk@gmail.com>
2018-03-23 21:02:40 +0300

    update, add fullscreen

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import {run} from './game/index.js';
+import styles from './index.css';
 
 const init = () => {
     if (window.app) {
@@ -9,22 +10,43 @@ const init = () => {
     window.app = run();
 };
 
-init();
-
 window.onresize = () => {
     init();
 };
 
-if (screen) {
-    try {
-        screen.orientation.lock('landscape');
-    } catch (err) {
-        console.error(err);
-    }
-}
+
 
 if (module.hot) {
     module.hot.accept(['./game/index.js'], () => {
         init();
     });
+}
+
+var button = document.createElement(`div`);
+button.innerHTML = "Start";
+document.body.appendChild(button);
+button.onclick = () => {
+    if (screen) {
+        try {
+            screen.orientation.lock('landscape');
+        } catch (err) {
+            console.error(err);
+        }
+    }
+    fullScreen(document.documentElement);
+
+    document.body.removeChild(button);
+    //init();
+}
+
+function fullScreen(element) {
+    if(element.requestFullscreen) {
+        element.requestFullscreen();
+    } else if(element.webkitrequestFullscreen) {
+        element.webkitRequestFullscreen();
+    }  else if(element.webkitRequestFullScreen) {
+        element.webkitRequestFullScreen();
+    }  else if(element.mozRequestFullscreen) {
+        element.mozRequestFullScreen();
+    }
 }
